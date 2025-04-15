ebf446d9168e9b8049aea0cabea73ea47a47f15e
ryota1228 <ryota.yamazaki@pathoslogos.co.jp>
2025-04-15 10:47:32 +0900

    一時コミット：Angularアップデート前

---
+++ src/app/pages/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavigationComponent } from '../components/navigation.component';
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [NavigationComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
